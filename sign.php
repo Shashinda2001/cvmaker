6e794ff46bf64f6eb55b83aa229f1c9c3bce1629
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2023-09-10 14:36:23 +0530

    version 1

---
+++ sign.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sign</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     <!-- //sign section -->
+     <section class="signbox">
+        
+
+        <form action="" method="POST" class="fombox" enctype="multipart/form-data" >
+        <h1 class="signhead">sign here</h1>
+        <input class="inputbox" type="text" name="userame" placeholder="User-Name" >
+        <input class="inputbox" type="mail" name="mail" placeholder="Mail" >
+        <input class="inputbox" type="password" name="password" placeholder="Password" >
+        <input type="file" name="image" class="inputbox" accept="image/jpg, image/jpeg, image/png">
+        <input class="btn" type="submit" name="submit" value="Sign">
+        <p class="pp1">already have an acount <a class="atag" href="login.php">cick here</a></p>
+        </form>
+
+
+
+     </section>
+    
+</body>
+</html>
